004ba47b1c67b33e1deacfcfebe9d6f27a6c59f3
jacky-git123 <liew93360@gmail.com>
2025-04-03 22:25:03 +0800

    Update customer.controller.ts

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,70 +1,277 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards, Headers } from '@nestjs/common';
-import { CustomerService } from './customer.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
 import { CreateCustomerDto, UpdateCustomerDto } from './customer.dto';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { LocalAuthGuard } from 'src/auth/local-auth.guard';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { pickBy } from 'lodash';
+import { count } from 'console';
+import { RunningNumberGenerator } from 'src/common/utils';
 
-@Controller('customer')
-export class CustomerController {
-    constructor(private readonly customerService: CustomerService) {}
+@Injectable()
+export class CustomerService {
+    constructor(private prisma: PrismaService,
+      private utilService:RunningNumberGenerator
+    ) {}
 
-  // @UseGuards(AuthGuard)
-  @Post()
-  async create(@Body() createCustomerDto: CreateCustomerDto, @Headers() headers: any, @Query('userid') userId: string) {
-    console.log(headers);
-    if (headers.auth_user) {
-      createCustomerDto.created_by = headers.user_id;
-    }else {
-      createCustomerDto.created_by = userId;
+  async create(data: any) {
+
+    if (data.ic && !data.id) {
+      const checkIC = await this.prisma.customer.findFirst({
+        where: {
+          ic: data.ic
+        }
+      });
+
+      if (checkIC && !data.id) {
+        throw new BadRequestException('IC already exist');
+      }
     }
-    return this.customerService.create(createCustomerDto);
+
+
+    if(data.passport && !data.id) {
+      const checkPass = await this.prisma.customer.findFirst({
+        where: {
+          passport: data.passport
+        }
+      });
+      if (checkPass) {
+        throw new BadRequestException('Passport already exist');
+      }
+    }
+
+    // console.log(data);
+    const { bankDetails, ...customerData } = data;
+    if(customerData.id) {
+      return this.updateCustomer(customerData, customerData.id);
+    } else {
+      const generate_id = await this.utilService.generateUniqueNumber('CT');
+      return this.addCustomer({ ...customerData, generate_id: generate_id });
+    }
   }
 
-  @Get()
-  async findAll(
-    @Query('skip') skip: number = 0,
-    @Query('take') take: number = 10,
-    @Query('filter') filter: any
-  ) {
-    return this.customerService.findAll(Number(skip), Number(take), filter);
+  async findAll(skip: number, take: number, filter: any) {
+    let where = {
+      deleted_at: null
+    }
+    if (filter) {
+      where = pickBy({
+        OR: [
+          {
+            name: {
+              contains: filter,
+              mode: "insensitive"
+            }
+          },
+          {
+            email: {
+              contains: filter,
+              mode: "insensitive"
+            }
+          },
+          {
+            passport: {
+              contains: filter,
+              mode: "insensitive"
+            }
+          },
+          {
+            ic: {
+              contains: filter,
+              mode: "insensitive"
+            }
+          }
+        ]
+      });
+    }
+    const [customers, total] = await Promise.all([
+      this.prisma.customer.findMany({
+        skip,
+        take,
+        where,
+      }),
+      this.prisma.customer.count({
+        where,
+      }),
+    ]);
+
+    return {
+      data: customers,
+      total,
+      skip,
+      take,
+    };
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return this.customerService.findOne(id);
+  async findOne(id: string) {
+    return this.prisma.customer.findUnique({
+      where: { id },
+      // include: {
+      //   customer_relation: {
+      //     include: {
+      //       address: true,
+      //     }
+      //   },
+      //   customer_address: true,
+      //   company: true,
+      // },
+    });
   }
 
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-    return this.customerService.update(id, updateCustomerDto);
+  async update(id: string, data: any) {
+    const { customer_relation, customer_address, company, bankDetails, ...customerData } = data;
+    return this.prisma.customer.update({
+      where: { id },
+      data: data,
+    });
   }
 
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
-    return this.customerService.delete(id);
+  async delete(id: string) {
+    return this.prisma.customer.update({
+      data: { deleted_at: new Date() },
+      where: { id },
+    });
   }
 
-  @Post('add-document')
-  addDocument(@Body() data: { id: string; filesData: any[] }) {
-    return this.customerService.addDocument(data);
-  }
-
-  @Get('getDocument/:key')
-  async getDocument(@Param('key') key: string) {
-    return this.customerService.getDocument(key);
-  }
-
+  async addDocument(data: { id: string; filesData: any[] }) {
+    const { id, filesData } = data;
   
-  @Get('getCustomer/:key')
-  async getCustomer(@Param('key') key: string) {
-    return this.customerService.getCustomer(key);
+    const promises = filesData.map(async (file) => {
+      return this.prisma.document.create({
+        data: {
+          name: file.fileName,
+          path: file.fileName,
+          customer_id: id, 
+          description: file.fileDescription,
+          size: file.fileSize.toString(),
+        },
+      });
+    });
+  
+    return Promise.all(promises);
+  }
+  
+
+  async addCustomer(data) {
+    if (data.customer_address) {
+      data.customer_address = JSON.parse(JSON.stringify(data.customer_address));
+    }
+    if (data.relations) {
+      data.relations = JSON.parse(JSON.stringify(data.relations));
+    }
+    if (data.employment) {
+      data.employment = JSON.parse(JSON.stringify(data.employment));
+    }
+    if (data.bank_details) {
+      data.bank_details = JSON.parse(JSON.stringify(data.bank_details));
+    }
+    return this.prisma.customer.create({
+      data: data,
+    });
   }
 
-  @Post('get-customer-status')
-  getCustomerStatus(
-    @Body() data: { key: string }
-  ) {
-    return this.customerService.getCustomerStatus(data.key);
+  async updateCustomer(data, id) {
+    if (data.customer_address) {
+      data.customer_address = JSON.parse(JSON.stringify(data.customer_address));
+    }
+    if (data.relations) {
+      data.relations = JSON.parse(JSON.stringify(data.relations));
+    }
+    if (data.employment) {
+      data.employment = JSON.parse(JSON.stringify(data.employment));
+    }
+    if (data.bank_details) {
+      data.bank_details = JSON.parse(JSON.stringify(data.bank_details));
+    }
+    return this.prisma.customer.update({
+      where: { id },
+      data: data,
+    });
   }
+
+  getCustomer(key: string) {
+    return this.prisma.customer.findMany({
+      where: pickBy({
+        OR: [
+          {
+            name: {
+              contains: key,
+              mode: "insensitive"
+            }
+          },
+          {
+            email: {
+              contains: key,
+              mode: "insensitive"
+            }
+          },
+          {
+            passport: {
+              contains: key,
+              mode: "insensitive"
+            }
+          },
+          {
+            ic: {
+              contains: key,
+              mode: "insensitive"
+            }
+          }
+        ]
+      }),
+    });
+  }
+
+  getDocument(key: string) {
+    return this.prisma.document.findMany({
+      where: { customer_id:key },
+    });
+  }
+
+  async getCustomerStatus(key: string) {
+    const customer = await this.prisma.customer.findFirst({
+      where: pickBy({
+        OR: [
+          {
+            email: {
+              contains: key,
+              mode: "insensitive"
+            }
+          },
+          {
+            ic: {
+              contains: key,
+              mode: "insensitive"
+            }
+          }
+        ]
+      }),
+    });
+    // Find Load
+    const loanStatusCounts = await this.prisma.loan.groupBy({
+      by: ['status', 'supervisor'],
+      where: {
+        customer_id: customer.id,
+      },
+      _count: {
+        status: true,
+        supervisor: true,
+      },
+    });
+    await Promise.all(loanStatusCounts.map(async (loan: any) => {
+      const supervisor = await this.prisma.user.findUnique({
+        where: {
+          id: loan.supervisor
+        }
+      });
+      loan.supervisorObj = supervisor;
+      if (loan.supervisor_2) {
+        const supervisor_2 = await this.prisma.user.findUnique({
+          where: {
+            id: loan.supervisor_2
+          }
+        });
+        loan.supervisor_2Obj = supervisor_2;
+      }
+    }));
+    return loanStatusCounts;
+  }
+  
 }
