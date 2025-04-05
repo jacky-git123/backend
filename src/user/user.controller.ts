fce469d393afe82b7320d920a879f924db4fff94
jacky-git123 <liew93360@gmail.com>
2025-04-05 09:42:19 +0800

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,7 @@ export class UserController {
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
+    delete createUserDto.userid;
     return this.userService.createUser(createUserDto);
   }
 
